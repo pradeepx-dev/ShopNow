9439136bf6f0793dfc568ede497f74592a3352d3
Pradeep Kumar Maurya <pradeepx135@gmail.com>
2026-09-05 02:30:50 +0530

    fix path error

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv').config()
 const express = require('express')
+const path = require('path')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 3000
